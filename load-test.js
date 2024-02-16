da999b57a81d9281b8b419e9a33798a7758c4a48
vishnu <vishnu1961993@gmail.com>
2024-02-16 17:35:31 +0530

    Update load-test.js

--- load-test.js
+++ load-test.js
@@ -1,9 +1,5 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-export const options = {
-  vus: 50,
-  duration: '30s',
-};
 export default function () {
   http.get('https://eusqaenvwebapp.azurewebsites.net');
   sleep(1);
